fdfbb2b5a2f4ef5ad3aeb7c0e79d4f4fdd2c0300
daikoku <jjys9047@naver.com>
2021-10-25 14:34:31 +0900

    변수, 상수

--- 기본문법/Study_1day.kt
+++ 기본문법/Study_1day.kt
@@ -1,8 +1,14 @@
-fun main(args: Array<String>) {
-    println("Hello, world!") 
-    }
-// fun : def in python
-// (args : Array<string>) : 함수인자 표기시 : 변수명 : 타입
-// println : print in python
+// val : 변경불가능한 값 == 상수(value)
+// var : 변경가능한 값 == 변수(variable)
 
+val result: Int = 3
+print(result) // 3
+// result = 4 // 컴파일 에러! val 변수는 대입 연산이 불가능하다!
 
+var mutableResult: Int = 3
+
+print(mutableResult) // 3
+mutableResult = 4 // var 변수는 대입 연산이 가능하다!
+print(mutableResult) // 4
+
+// var someVariable: Int // 컴파일 에러! 초기화를 해주지 않으면 에러다.
